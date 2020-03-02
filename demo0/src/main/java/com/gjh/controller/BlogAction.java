852c7054f242ba256313d5feacb52f1f16b35b55
gjh <1138372894@qq.com>
2020-03-02 20:51:34 +0800

    业务逻辑层

---
+++ demo0/src/main/java/com/gjh/controller/BlogAction.java
@@ -0,0 +1,156 @@
+package com.gjh.controller;
+
+import com.gjh.dao.BlogDao;
+import com.gjh.entity.Blog;
+import com.gjh.entity.User;
+import com.gjh.service.*;
+import com.gjh.vo.BlogQuery;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.stereotype.Controller;
+
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpSession;
+import java.util.Date;
+import java.util.List;
+
+@Controller
+
+public class BlogAction {
+    @Resource
+    private BlogService blogService;
+    @Resource
+    private BlogTypeService blogTypeService;
+    @Resource
+    private AttentionService attentionService;
+
+    @Resource
+    private UserService userService;
+    @Resource
+    private BlogTagService blogTagService;
+
+    @PostMapping("bsave")//保存博客
+    public String save(Blog blog ,Model model)throws Exception
+    {
+        if(blog.getBflag()==""||blog.getBflag()==null){
+            blog.setBflag("原创");
+        }
+
+        blog.setTags(blogTagService.listTag(blog.getTagIds()));
+
+        blogService.saveBlog(blog);
+
+        return "redirect:/blogget";
+
+    }
+    @RequestMapping("/editor")//写博客
+    public String editor(Model model)
+    {
+        setTypeAndTag(model);
+        model.addAttribute("blog",new Blog());
+
+        return "blog-text";
+    }
+    @RequestMapping("/redit/{uid}")//博客编辑
+    public String redit(Model model,@PathVariable long uid)
+    {
+        setTypeAndTag(model);
+        Blog blog=blogService.getBlog(uid);
+        blog.init();
+        model.addAttribute("blog",blog);
+        return "blog-text";
+    }
+    private void setTypeAndTag(Model model)//初始化类型和标签
+    {
+        model.addAttribute("typeall",blogTypeService.listType());
+        model.addAttribute("tagall",blogTagService.listTag());
+    }
+    @RequestMapping("/blogs/delect")//博客删除
+    @ResponseBody
+    public String delectBlog(long bid, RedirectAttributes attributes)
+    {
+
+        Blog blog=blogService.getBlog(bid);
+        blog.setBpublished("0");
+        blogService.updateBlog(bid,blog);
+
+      attributes.addFlashAttribute("message","删除成功");
+        return "1";
+    }
+
+
+
+    @RequestMapping("/blogget")//个人博客
+    public String blogget(Model model, @PageableDefault(size = 5,sort = {"bupdateTime"},direction = Sort.Direction.DESC)Pageable pageable, BlogQuery blog, HttpSession session)throws Exception
+    {
+        blog.setPublished("all");//除了删除的全部博客
+        System.out.println(blog);
+       User u=(User)session.getAttribute("user");
+        blog.setUid(u.getUid());
+        model.addAttribute("blogList",blogService.listBlog(pageable,blog));
+        model.addAttribute("blogs",blog);
+        model.addAttribute("listtype",blogTypeService.listType());
+        return "myblogs";
+    }
+    @RequestMapping("youindex/{uid}")//主页
+    @Transactional
+    public String youindex(Model model, @PageableDefault(size = 5,sort = {"bupdateTime"},direction = Sort.Direction.DESC)Pageable pageable, BlogQuery blog,@PathVariable long uid,HttpSession session)throws Exception
+    {
+        if (session.getAttribute("user")!=null){
+            User u=(User)session.getAttribute("user");
+            Long lid=u.getUid();
+            model.addAttribute("lid",lid);//用来前端页面判断是否显示关注按钮使用
+        }
+       blog.setPublished("1");//发布的博客
+        blog.setUid(uid);
+        User user=userService.getUid(uid);
+        int myFans=attentionService.getFans(uid,"1");//查询粉丝人数
+        int myFollow=attentionService.getFollow(uid,"1");//查询关注人数
+        model.addAttribute("myuser",user);
+        model.addAttribute("myFans",myFans);
+        model.addAttribute("myFollow",myFollow);
+        model.addAttribute("yblogList",blogService.listBlog(pageable,blog));
+        return "your-index";
+    }
+    @RequestMapping("persiontext")//个人中心
+    public String persiontext(HttpSession session,Model model)throws Exception{
+        User u=(User)session.getAttribute("user");
+        int myFans=attentionService.getFans(u.getUid(),"1");
+        int myFollow=attentionService.getFollow(u.getUid(),"1");
+        model.addAttribute("myFans",myFans);
+        model.addAttribute("myFollow",myFollow);
+        return "homepage";
+    }
+    @RequestMapping("follow/{uid}")//他的关注
+    @Transactional
+    public String follow(Model model ,@PathVariable long uid,HttpSession session)throws Exception
+    {
+        if (session.getAttribute("user")!=null){
+            User u=(User)session.getAttribute("user");
+            Long lid=u.getUid();
+            model.addAttribute("lid",lid);
+        }
+
+
+        User user=userService.getUid(uid);
+        int myFans=attentionService.getFans(uid,"1");
+        int myFollow=attentionService.getFollow(uid,"1");
+        model.addAttribute("myuser",user);
+        model.addAttribute("myFans",myFans);
+        model.addAttribute("myFollow",myFollow);
+        model.addAttribute("myfollows", attentionService.getByfid(uid));
+
+        return "your-index-follow";
+    }
+
+
+
+
+
+}
---
+++ demo0/src/main/java/com/gjh/controller/UserAction.java
@@ -0,0 +1,128 @@
+package com.gjh.controller;
+
+
+import com.gjh.entity.User;
+import com.gjh.service.UserService;
+
+import org.springframework.data.redis.core.StringRedisTemplate;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import javax.annotation.Resource;
+
+import javax.servlet.http.HttpSession;
+import java.io.File;
+import java.util.UUID;
+import java.util.concurrent.TimeUnit;
+
+
+@Controller
+@RequestMapping("douser")
+public class UserAction {
+
+    @Resource
+    private UserService userService;
+    @Resource
+    StringRedisTemplate stringRedisTemplate;
+
+    @RequestMapping("gfile")//照片上传
+    public String gfile(MultipartFile file,HttpSession session) throws Exception
+    {
+
+        String extName=file.getOriginalFilename().substring(file.getOriginalFilename().lastIndexOf("."));
+        String fileName= UUID.randomUUID().toString()+extName;
+
+//        String filepath="/root/img";//linux地址
+        String filepath="F:\\upload\\";
+        File dest=new File(filepath + fileName);
+        file.transferTo(dest);
+        User u=(User)session.getAttribute("user");
+        u.setUheadimg(fileName);
+        userService.add(u);
+
+        return "redirect:/persiontext";
+
+    }
+    @GetMapping("login")
+    @ResponseBody
+    public String login(String uemail,String upwd, HttpSession session)throws Exception
+    {
+
+        User u=userService.login(uemail,upwd);
+        System.out.println(u);
+        if(u!=null)
+        {
+            session.setAttribute("user",u);
+            return "1";
+        }else
+            {
+                return "0";
+
+            }
+    }
+
+    @GetMapping("yan")
+    @ResponseBody
+    public String yan()throws Exception
+    {
+        int random=(int)(Math.random()*9999)+10000;
+        String random1=String.valueOf(random);
+        stringRedisTemplate.opsForValue().set("yan",random1,60, TimeUnit.SECONDS);
+        return random1;
+    }
+
+    @GetMapping("register")
+    @ResponseBody
+    @Transactional
+    public String register(User user,String num)throws Exception
+    {
+        User ue=userService.reEmail(user.getUemail());
+        String num1=stringRedisTemplate.opsForValue().get("yan");
+        if(user==null||user.equals(""))
+        {
+            return "0";
+        }else if(!num.equals(num1))
+        {
+            return "2";
+      }else if(ue!=null)
+      {
+           return "3";       }
+
+        boolean u= userService.register(user);
+        if (u==true)
+        {
+            return "1";
+        }else
+        {
+            return "0";
+        }
+
+    }
+
+
+
+
+
+
+
+
+
+    @GetMapping("out")
+    @ResponseBody
+ public String out(HttpSession session)throws Exception
+    {
+        session.removeAttribute("user");
+
+        return "0";
+    }
+
+
+
+}
