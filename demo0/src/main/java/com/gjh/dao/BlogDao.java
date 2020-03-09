e3ceea0b2d162d9ab3be28d073716580ee52633d
gjh <1138372894@qq.com>
2020-03-09 19:50:36 +0800

    更新

--- demo0/src/main/java/com/gjh/dao/BlogDao.java
+++ demo0/src/main/java/com/gjh/dao/BlogDao.java
@@ -1,10 +1,17 @@
 package com.gjh.dao;
 
 import com.gjh.entity.Blog;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface BlogDao extends JpaRepository<Blog,Long>,JpaSpecificationExecutor<Blog> {
-Blog findByBid(Long id);
+    Blog findByBid(Long id);
+
+    @Query("select b from Blog b where b.bpublished = '1'")//最新推荐
+      List<Blog> findNew(Pageable pageable);
 
 }
