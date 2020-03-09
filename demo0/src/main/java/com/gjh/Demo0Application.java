8cff8303d1645fb16c735256780257f960283404
gjh <1138372894@qq.com>
2020-03-09 19:54:48 +0800

    启动类

---
+++ demo0/src/main/java/com/gjh/Demo0Application.java
@@ -0,0 +1,18 @@
+package com.gjh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+@SpringBootApplication
+
+public class Demo0Application{
+
+	public static void main(String[] args)
+		{
+		SpringApplication.run(Demo0Application.class, args);
+	}
+
+
+}
